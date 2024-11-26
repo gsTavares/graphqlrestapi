e90bc81cc880e2e73f74681a3982dea1578d314d
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-11-26 19:38:42 -0300

    first commit

---
+++ src/main/java/com/example/graphqlrestapi/GraphqlrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/graphqlrestapi/GraphqlrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqlrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
